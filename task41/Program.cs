ecc3176128509fd8fa9df6d3fb84e12712ea970c
Anya Denisova <bilberian@mail.ru>
2022-11-30 22:26:55 +0300

    upd41

--- task41/Program.cs
+++ task41/Program.cs
@@ -5,25 +5,32 @@
 
 // 1, -7, 567, 89, 223-> 3
 
-int[] FormArray(int size)
-{
-    int[] arr = new int[size];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = new Random().Next(-1000, 1000);
-    }
-    return arr;
-}
+// int[] FormArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(-1000, 1000);
+//     }
+//     return arr;
+// }
 
-Console.WriteLine("Введите количество элементов для массива: ");
-int count = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-int[] array = FormArray(count);
-Console.Write("[" + string.Join(", ", array) + "]");
-Console.WriteLine();
+// Console.WriteLine("Введите количество элементов для массива: ");
+// int count = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+// int[] array = FormArray(count);
+// Console.Write("[" + string.Join(", ", array) + "]");
+// Console.WriteLine();
+
+Console.WriteLine("Введите числа (через пробел): ");
+string numbersList = Console.ReadLine();
+numbersList = numbersList.TrimEnd().TrimStart();
+string[] array = numbersList.Split(null);
+int[] arrayInt = Array.ConvertAll(array, int.Parse);
+
 
 int counter = 0;
-for (int i = 0; i < count; i++)
+for (int i = 0; i < arrayInt.Length; i++)
 {
-    if(array[i] > 0) counter++;
+    if(arrayInt[i] > 0) counter++;
 }
 Console.WriteLine(counter);
